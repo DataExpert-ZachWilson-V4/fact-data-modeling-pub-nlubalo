230d28b333b539710b43f75bab6bea283eb8e509
nlubalo <nancyatienno@gmail.com>
2024-05-19 13:19:30 +0300

    fix error

--- submission/query_7.sql
+++ submission/query_7.sql
@@ -1,7 +1,7 @@
 -- Create a table named host_activity_reduced
 CREATE TABLE nancyatienno21998.host_activity_reduced (
     host VARCHAR,
-    metric_name VARCHAR
+    metric_name VARCHAR,
     metric_array ARRAY(INTEGER),
     month_start VARCHAR
 )
