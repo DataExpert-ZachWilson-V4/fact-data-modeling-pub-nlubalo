6592137e42c2b93748ac6da3fa0c9c2956f43c6d
nlubalo <nancyatienno@gmail.com>
2024-05-19 12:37:40 +0300

    update date

--- submission/query_3.sql
+++ submission/query_3.sql
@@ -9,7 +9,7 @@ WITH
     FROM
       user_devices_cumulated
     WHERE
-      DATE = DATE('2021-01-01')
+      DATE = DATE('2021-01-17')
   ),
   -- CTE to fetch data from today (2021-01-18)
   today AS (
@@ -22,7 +22,7 @@ WITH
       bootcamp.web_events AS we
     LEFT JOIN bootcamp.devices AS d ON d.device_id = we.device_id
     WHERE
-      date_trunc('day', we.event_time) = DATE('2021-01-02')
+      date_trunc('day', we.event_time) = DATE('2021-01-18')
     GROUP BY
       we.user_id,
       d.browser_type,
@@ -36,7 +36,7 @@ SELECT
     WHEN y.dates_active IS NOT NULL THEN ARRAY[t.event_date] || y.dates_active  -- If there are active dates from yesterday, append today's date
     ELSE ARRAY[t.event_date]    -- If there are no active dates from yesterday, create a new array with today's date
   END AS dates_active,          -- Determine the dates_active array
-  DATE('2021-01-02') AS date    -- Set the date as '2021-01-18' for the new row
+  DATE('2021-01-18') AS date    -- Set the date as '2021-01-18' for the new row
 FROM
   yesterday y                   -- Left table of the FULL OUTER JOIN
   FULL OUTER JOIN today t      -- Right table of the FULL OUTER JOIN
